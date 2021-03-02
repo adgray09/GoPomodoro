5a7cb2db58f3964ed1814a957fe1dee718903b2a
GraySpace <alexandergray37@gmail.com>
2021-03-02 18:31:48 -0500

    not working but can create work timer so far

--- main.go
+++ main.go
@@ -14,10 +14,13 @@ import (
 
 func main() {
 
-	duration := flag.Duration("duration", 25*time.Minute, "Pomodoro duration")
+	workDuration := flag.Duration("work", 25*time.Minute, "Work duration - default: 25 minutes")
+	breakDuration := flag.Duration("break", 5*time.Minute, "Break duration - default: 5 minutes")
 	flag.Parse()
 
-	startTimer(*duration)
+	workTimer(*workDuration)
+	breakTimer(*breakDuration)
+
 }
 
 func alarm() {
@@ -55,14 +58,81 @@ func createTimer(timerDuration time.Duration, action func()) *time.Timer {
 	return timer
 }
 
-func startTimer(timerDuration time.Duration) {
+func workTimer(timerDuration time.Duration) {
 	timer := createTimer(timerDuration, func() {
-		fmt.Println("Take a break!")
-		alarm()
 	})
-	defer timer.Stop()
 
-	killTimer := (time.Duration(timerDuration) + time.Second)
-	countdownBeforeExit := time.NewTimer(time.Second * killTimer)
-	<-countdownBeforeExit.C
+	defer func() {
+		select {
+		case <-timer.C:
+		default:
+			alarm()
+			fmt.Println("Take a break! Start your break timer.")
+			// newDuration := 5 * time.Second
+			// breakTimer(newDuration)
+		}
+	}()
+	<-timer.C
+
+}
+
+func breakTimer(timerDuration time.Duration) {
+	timer := createTimer(timerDuration, func() {
+	})
+
+	defer func() {
+		timer.Stop()
+		select {
+		case <-timer.C:
+		default:
+			alarm()
+			fmt.Println("Break over! start work timer!")
+		}
+	}()
+	<-timer.C
+}
+
+// Future implementation ?
+// func testingTicker() {
+// 	ticker := time.NewTicker(5 * time.Second)
+// 	ticker2 := time.NewTicker(30 * time.Second)
+// 	done := make(chan bool)
+
+// 	go func() {
+// 		for {
+// 			select {
+// 			case <-done:
+// 				return
+// 			case t := <-ticker.C:
+// 				fmt.Println("Tick at", t)
+// 				alarm()
+// 			case t2 := <-ticker2.C:
+// 				fmt.Println("Ticker 2", t2)
+// 			}
+// 		}
+// 	}()
+
+// 	time.Sleep(1 * time.Hour)
+// 	ticker.Stop()
+// 	done <- true
+// 	fmt.Println("Ticker stopped")
+// }
+
+func showTimeLeft(timerDuration time.Duration) {
+	// not currently implemented
+	now := time.Now()
+	end := now.Add(timerDuration)
+
+	for range time.Tick(1 * time.Second) {
+		newNow := time.Now()
+
+		dif := end.Sub(newNow)
+
+		total := int(dif.Seconds())
+
+		minutes := int(total/60) % 60
+		seconds := int(total % 60)
+
+		fmt.Println(minutes, seconds)
+	}
 }
